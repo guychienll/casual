d238a5725502e0bb002e12997d12d9dc31818480
guychienll <guy.chien@pinkoi.com>
2023-10-22 21:26:17 +0800

    docs: how to add a custom eslint rule wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,11 +72,10 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'NotesSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Notes',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
