4e2fc492730667a0e7708b5a994531e5deec05a6
guychienll <guy.chien@pinkoi.com>
2023-10-26 23:54:51 +0800

    build: blog javascript hoisting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,7 @@ const config = {
             position: 'left',
             label: 'Notes',
           },
+          {to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       // footer: {
