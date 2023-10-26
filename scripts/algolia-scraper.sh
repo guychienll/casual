f1d171e63ebe112ba72639ec50fe01609cc1db7d
guychienll <guy.chien@pinkoi.com>
2023-10-26 22:37:15 +0800

    build: remove it

--- scripts/algolia-scraper.sh
+++ scripts/algolia-scraper.sh
@@ -1,1 +1,1 @@
-docker run --platform linux/amd64 -it --env-file=.env -e "CONFIG=$(cat ./config.json | jq -r tostring)" algolia/docsearch-scraper
+docker run --platform linux/amd64 --env-file=.env -e "CONFIG=$(cat ./config.json | jq -r tostring)" algolia/docsearch-scraper
