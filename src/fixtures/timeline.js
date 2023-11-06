12c96d05204b36d7575161d0ed620ce4c0dfea89
guychienll <guychienll@gmail.com>
2023-11-06 22:57:30 +0800

    Revert "style: fade in image"
    This reverts commit 87e443139ca028cf068a3418362085819117a187.

--- src/fixtures/timeline.js
+++ src/fixtures/timeline.js
@@ -1,50 +1,17 @@
 import clsx from 'clsx';
-import React, { useState } from 'react';
+import React from 'react';
 import Zoom from 'react-medium-image-zoom';
-import styled from 'styled-components';
 
-const LazyImage = styled.div`
-    width: 100%;
-    aspect-ratio: 16/9;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    & > img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-        opacity: 0;
-        &.visible {
-            opacity: 1;
-        }
-        transition: opacity 500ms ease-in;
-    }
-`;
-
-const Image = ({ src, alt, lazy = true, style = {} }) => {
-    const [loaded, setLoaded] = useState(false);
-    return (
-        <Zoom>
-            <LazyImage>
-                <img
-                    style={{
-                        ...style,
-                    }}
-                    className={clsx({
-                        visible: loaded,
-                    })}
-                    src={src}
-                    alt={alt}
-                    {...(lazy ? { loading: 'lazy' } : {})}
-                    onLoad={() => {
-                        setLoaded(true);
-                    }}
-                />
-            </LazyImage>
-        </Zoom>
-    );
-};
+const Image = ({ src, alt, lazy = true, style = {} }) => (
+    <Zoom>
+        <img
+            style={style}
+            src={src}
+            alt={alt}
+            {...(lazy ? { loading: 'lazy' } : {})}
+        />
+    </Zoom>
+);
 const ProjectCard = (props) => {
     const {
         title = '',
