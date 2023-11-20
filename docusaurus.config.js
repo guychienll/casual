24f50c5d0659d0d95d34c808171022c350e396e8
guychienll <guychienll@gmail.com>
2023-11-20 10:58:12 +0800

    docs: typescript 003 update truthiness narrowing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,6 @@ const config = {
             };
         },
     ],
-
     presets: [
         [
             'classic',
@@ -108,7 +107,10 @@ const config = {
             },
         ],
     ],
-
+    markdown: {
+        mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
     themeConfig: {
         // Replace with your project's social card
         // image: 'img/docusaurus-social-card.jpg',
