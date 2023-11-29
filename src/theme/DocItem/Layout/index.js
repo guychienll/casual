d83861fa50b1fb9505e0da58de3e7885cacbf896
guychienll <guychienll@gmail.com>
2023-11-29 14:04:39 +0800

    fix: remove useless draft flag

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -36,7 +36,6 @@ export default function DocItemLayout({ children }) {
     const docTOC = useDocTOC();
     const { metadata } = useDoc();
     const { frontMatter, unlisted } = metadata;
-    const { draft } = frontMatter;
 
     React.useEffect(() => {
         const header = document.querySelector('head');
@@ -79,14 +78,7 @@ export default function DocItemLayout({ children }) {
                         <DocBreadcrumbs />
                         <DocVersionBadge />
                         {docTOC.mobile}
-                        <DocItemContent>
-                            {draft && (
-                                <div className="alert alert--warning margin-bottom--md">
-                                    編輯中 Working in Progress
-                                </div>
-                            )}
-                            {children}
-                        </DocItemContent>
+                        <DocItemContent>{children}</DocItemContent>
                         <DocItemFooter />
                         <Discussion />
                     </article>
